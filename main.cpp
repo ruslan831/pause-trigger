1f6d2c9d0bf96cd1143b84885ae1dfab6e7364d6
RusMus <66568030+ruslan831@users.noreply.github.com>
2025-01-23 18:19:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
-auto ismod = Mod::get()->getSettingValue<bool>("power");
 
 #include <Geode/modify/PlayerObject.hpp>
 
 class $modify(PlayerObject) {
     void update(float p0) {
-        if(ismod == true)
         if (m_playerSpeed == 0.7f)
         m_playerSpeed = 0;
         PlayerObject::update(p0);
     }
-
 };
